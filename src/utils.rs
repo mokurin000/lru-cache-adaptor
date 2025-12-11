c85a28baffdda77a32a1afaad797a26c8b674d02
莯凛 <mokurin000@gmail.com>
2025-12-11 14:42:31 +0800

    enhance: handle errors like permission denied

--- src/utils.rs
+++ src/utils.rs
@@ -1,13 +1,14 @@
-use std::path::Path;
+use std::{io::ErrorKind, path::Path};
 
 use crate::LRUResult;
 
 pub fn remove_file_get_size(file: impl AsRef<Path>) -> LRUResult<Option<u64>> {
     let path = file.as_ref();
     let filesize = path.metadata()?.len();
-    if std::fs::remove_file(path).is_ok() {
-        Ok(Some(filesize))
-    } else {
-        Ok(None)
+
+    match std::fs::remove_file(path) {
+        Ok(_) => Ok(Some(filesize)),
+        Err(e) if e.kind() == ErrorKind::NotFound => Ok(None),
+        Err(e) => Err(e)?,
     }
 }
